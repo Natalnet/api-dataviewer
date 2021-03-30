801e6870aebdf5d82fdace21efad856136144573
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2021-03-29 22:19:29 -0300

    Add files via upload

--- scripts/debugs/update_db_teste.py
+++ scripts/debugs/update_db_teste.py
@@ -16,6 +16,16 @@ from classes.manage_db import Manage_db
 lop = Lop()		
 psql = Manage_db()
 
+#Endpoints
+os.environ['ENDPOINT_ALL_CLASSES'] = 'https://api.lop.natalnet.br:3001/dataScience/class?key='
+os.environ['ENDPOINT_ALL_LISTS'] = 'https://api.lop.natalnet.br:3001/dataScience/list?key='
+os.environ['ENDPOINT_ALL_QUESTIONS'] = 'https://api.lop.natalnet.br:3001/dataScience/question?key='
+os.environ['ENDPOINT_ALL_SUBMISSIONS'] = 'https://api.lop.natalnet.br:3001/dataScience/submission?key='
+os.environ['ENDPOINT_ALL_TESTS'] = 'https://api.lop.natalnet.br:3001/dataScience/test?key='
+os.environ['ENDPOINT_TEACHER']  = 'https://api.lop.natalnet.br:3001/dataScience/teacher?key='
+os.environ['SECRET_KEY'] = 'd41d8cd98f00b204e9800998ecf8427e'
+
+
 #Essa função verifica a base de dados e definirá a data em que
 #será realizada a consulta dessa tabela. Caso a tabela esteja vazia
 #o retorno será a primeira data que se tem cadastro no lop
@@ -23,7 +33,7 @@ psql = Manage_db()
 #ultimo ultimo registro
 def verify_database(name_table):
 	print('Verificando a última data da tabela ' + name_table)
-	condition = 'ORDER BY "createdAt" DESC LIMIT 1'
+	condition = 'WHERE "createdAt" IS NOT NULL ORDER BY "createdAt" DESC LIMIT 1'
 	#Consulta no db
 	query = psql.search(table=name_table, columns='"createdAt"', condition=condition)
 	if query.empty == True:
@@ -191,7 +201,7 @@ def update_db():
 		schedule.every().day.at('11:42').do(update_lists)
 		schedule.every().day.at('11:43').do(update_tests)
 		schedule.every().day.at('11:44').do(update_teachers_classes)
-		schedule.every().day.at('12:17').do(update_questions)		
+		schedule.every().day.at('22:17').do(update_questions)		
 		schedule.every().day.at('15:47').do(update_submissions)
 		#Para não chamar a função mais uma vez, é inserido um intervalo de tempo
 		#para a próxima chamada de função
