061e5f6e72d1c0fb83d70719cdc38140a8f50ae6
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2021-03-30 23:49:33 -0300

    Add files via upload

--- container-updatedb/update_db.py
+++ container-updatedb/update_db.py
@@ -14,17 +14,7 @@ from classes.manage_db import Manage_db
 
 #Instanciando as classes
 lop = Lop()		
-psql = Manage_db()
-
-#Endpoints
-os.environ['ENDPOINT_ALL_CLASSES'] = 'https://api.lop.natalnet.br:3001/dataScience/class?key='
-os.environ['ENDPOINT_ALL_LISTS'] = 'https://api.lop.natalnet.br:3001/dataScience/list?key='
-os.environ['ENDPOINT_ALL_QUESTIONS'] = 'https://api.lop.natalnet.br:3001/dataScience/question?key='
-os.environ['ENDPOINT_ALL_SUBMISSIONS'] = 'https://api.lop.natalnet.br:3001/dataScience/submission?key='
-os.environ['ENDPOINT_ALL_TESTS'] = 'https://api.lop.natalnet.br:3001/dataScience/test?key='
-os.environ['ENDPOINT_TEACHER']  = 'https://api.lop.natalnet.br:3001/dataScience/teacher?key='
-os.environ['SECRET_KEY'] = 'd41d8cd98f00b204e9800998ecf8427e'
-
+psql = Manage_db(database = 'dataview_bd')
 
 #Essa função verifica a base de dados e definirá a data em que
 #será realizada a consulta dessa tabela. Caso a tabela esteja vazia
@@ -32,6 +22,7 @@ os.environ['SECRET_KEY'] = 'd41d8cd98f00b204e9800998ecf8427e'
 #caso exista dados na tabela, a consulta deve buscar a data do
 #ultimo ultimo registro
 def verify_database(name_table):
+	print('Verificando a última data da tabela ' + name_table)
 	condition = 'WHERE "createdAt" IS NOT NULL ORDER BY "createdAt" DESC LIMIT 1'
 	#Consulta no db
 	query = psql.search(table=name_table, columns='"createdAt"', condition=condition)
@@ -57,14 +48,13 @@ def insert_in_db(df, name_table):
 		elif name_table == 'submissions':
 			#Removendo valores NaN e trocando por 0
 			df = df.fillna(0)
-		try:			
+		try:
 			#Tenta inserir no bd
 			psql.insert_df(table = name_table, df=df)
 			return
 		except Exception as e:
 			#Caso a inserção não de certo
 			#pensar em algo como enviar por email
-			#print('Erro em inserir na tabela de ' + name_table + ' ' + str(e))
 			return
 
 
@@ -93,11 +83,16 @@ def update_teachers_classes():
 	key = os.environ['SECRET_KEY']
 	endpoint_all_classes = os.environ['ENDPOINT_ALL_CLASSES']
 	endpoint_teacher = os.environ['ENDPOINT_TEACHER'] 
-	#Consulta
-	df = lop.lop_class_db(endpoint_all_classes, endpoint_teacher, key, date)
-	#Insere os dados novos na tabela respectiva no db
-	insert_in_db(df, name_table)
-	return
+	try:
+		#Consulta
+		df = lop.lop_class_db(endpoint_all_classes, endpoint_teacher, key, date)
+	except Exception as e:
+		raise ValueError('Error: consult in lop. ' + str(e))
+	try:
+		#Insere os dados novos na tabela respectiva no db
+		insert_in_db(df, name_table)
+	except Exception as e:
+		raise ValueError('Error: insert in db. ' + str(e))
   	
 def update_submissions():
 	name_table = 'submissions'
@@ -108,13 +103,15 @@ def update_submissions():
 	endpoint_all_submissions = os.environ['ENDPOINT_ALL_SUBMISSIONS']
 	#Coletando a data atual para usar como limite
 	actual_date = datetime.now()
-	#Convertendo para o formato americano de datas
-	actual_date = actual_date.strftime('%Y-%m-%d %H:%M:%S')
+	#Retira problemas para a conversão de data
+	date = date.replace('T',' ').split('.')[0]
+	#Converte para o formato
+	date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
 	#Condição para impedir que a função faça consulta na API quando o intervalo de tempo é menor que 0 dias
 	#Já que o range de datas está configurado pra gerar intervalo de 1 dia, deixar passar menos que isso 
 	#o código da erro
 	#O if verifica se o intervalo de tempo for igual a 0 dias, se for, retorne
-	if (actual_date - datetime.strptime(date, '%Y-%m-%d  %H:%M:%S')).days == 0:
+	if (actual_date - date).days == 0:
 		return
 	#Gerando intervalo de tempo de 3 dias em dataframes
 	df_dates = pd.date_range(start = date, end = actual_date, freq = 'D', closed = None)
@@ -129,9 +126,9 @@ def update_submissions():
   		#Flag, indica que a consulta deu certo
   		requisition_accepted = False
   		while j != 5:
-  			try:  			
+  			try:
   				#Consulta na API
-  				df = lop.lop_submission_db(endpoint_all_submissions, key, date, date_limit)
+  				df = lop.lop_submission_db(endpoint_all_submissions, key, date, date_limit)  			
   				#Insere os dados novos na tabela respectiva no db
   				insert_in_db(df, name_table)
   				#Se funcionar o iterador recebe 5 que é o valor que sai do laço de tentativas
@@ -139,15 +136,14 @@ def update_submissions():
   				#Mudamos o estado da flag pra mostrar que a consulta foi realizada com sucesso
   				requisition_accepted = True  					
   			except Exception as e:
-  				#print('erro foi ' + str(e))
   				#Incrementador que limita o número de tentativas
   				j = j + 1
   		#Se a consulta não foi aceita		
   		if requisition_accepted == False:
-  			#***** local de futuro envio de email  		
+  			#***** local de futuro envio de email
   			return
   		else:		
-  			print('Requisição realizada com sucesso')
+  			return
 
 def update_lists():
 	name_table = 'lists'
@@ -178,11 +174,12 @@ def update_tests():
 #Função que atualiza os dados conforme a data e a hora escolhida
 def update_db():
 	try:
+		print('Na função enquanto não é chegada a hora da tarefa programada')
 		schedule.every().day.at('11:42').do(update_lists)
 		schedule.every().day.at('11:43').do(update_tests)
 		schedule.every().day.at('11:44').do(update_teachers_classes)
-		schedule.every().day.at('12:17').do(update_questions)		
-		schedule.every().day.at('15:47').do(update_submissions)
+		schedule.every().day.at('22:17').do(update_questions)		
+		schedule.every().day.at('04:00').do(update_submissions)
 		#Para não chamar a função mais uma vez, é inserido um intervalo de tempo
 		#para a próxima chamada de função
 		time.sleep(20)
@@ -190,6 +187,7 @@ def update_db():
 	    return 'Error: update function' + str(e)
 
 while True:
-	update_db()
+	#update_db()
+	update_submissions()
 	schedule.run_pending()
 	time.sleep(1)
