b6c086019d6ecc60acaa28ee0556e3367662f51f
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2021-03-31 23:35:54 -0300

    Add files via upload

--- classes/manage_authentication.py
+++ classes/manage_authentication.py
@@ -1,61 +1,71 @@
 from passlib.hash import pbkdf2_sha512
 from classes.manage_db import Manage_db
+import pandas as pd
 
-psql = Manage_db(database = 'dataviewer_users')
+psql = Manage_db(database = 'dataviewer_users', host = 'localhost')
 
 class Authentication:
 
 	def __init__(self, table):
 		self.table = table
 
-	#Gera uma senha em hash
+	#Gera um hash para a senha
 	def generate_hash(self, password):
 		return pbkdf2_sha512.hash(password)
 
 	#Verifica se as senhas hash que vem do json e do banco são iguais
 	def verify_hash(self, password_json, password_db):
-		return pbkdf2_sha512.verify(password_db, password_json)
+		return pbkdf2_sha512.verify(password_json, password_db)
 
 	#Insere novo usuário no banco de dados
-	def insert_new_user(self, user_json, password_json):
-		condition = f'WHERE "user" = {user_json}'
-		#Consulta no banco de dados se o usuário existe
-		query = psql.search(table = self.table, columns = '"password_hash"', condition = condition)
-		#Se o retorno for vazio então o usuário não existe no banco
+	def insert_new_user(self, username_json, password_json, list_datas, list_labels):
+		#Verifica se esse usuário existe no banco
+		query = self.verify_user(username_json)
+		#Se o usuário não existir no banco
 		if query.empty == True:
 			#Gera um hash correspondente a senha
 			password_hash = self.generate_hash(password_json)
-			#Cria o dataframe com os dados
-			df = pd.DataFrame([user_json, password_hash], columns = ['user','password_hash'])
-			try:
-				#Insere na tabela
-				pqsl.insert_df(table = self.table, df = df)
-				return True
-			except Exception as e:
-				raise ValueError('Error: insert df in table. ' + str(e))
+			#Insere a senha na lista
+			list_datas.append(password_hash)
+			#Cria um dataframe a partir das listas
+			df = pd.DataFrame([list_datas], columns = list_labels)	
+			#Insere no banco de dados
+			psql.insert_df(table = self.table, df = df)
+			return
 		else:
-			raise ValueError('Error: username already exists in database')
+			raise ValueError('Error: insert df in table. ' + str(e))
 
-	#Verifica se esse usuário com esse nome e senha existe no banco
-	def verify_user(self, user_json, password_json):
-		try:
-			condition = f'WHERE "user" = {user_json}'
-			#Consulta no banco de dados se o usuário existe
-			query = psql.search(table = self.table, columns = '"password_hash"', condition = condition)
-			#Se o retorno for vazio então o usuário não existe no banco
-			if query.empty == True:
-				raise ValueError('Error: User does not exist.')
+	#Verifica se esse usuário existe no banco
+	def verify_user(self, username_json):
+		#Insere aspas
+		username_string = "'" + username_json + "'"
+		#Monta query
+		condition = f'WHERE "username" = {username_string} '
+		#Consulta no banco de dados se o usuário existe
+		query = psql.search(table = self.table, condition = condition)
+		return query		
+
+	#Essa função autentica um usuário. Ela verifica primeiro se o usuário existe no banco
+	#e em seguida verifica se a senha que está no banco de dados corresponde a senha
+	#que veio no json
+	def authenticate_user(self, username_json, password_json):
+		print(username_json)
+		query = self.verify_user(username_json)
+		print('resultado da query')
+		print(query)
+		print('fim do resultado')
+		#Se o retorno for vazio então o usuário não existe no banco
+		if query.empty == True:
+			raise ValueError('Error: User does not exist.')
+		else:
+			#Lendo a password correspondente 
+			password_db = query['password'][0]
+			#Verifica se as senhas são iguais
+			if self.verify_hash(password_json, password_db):
+				return
 			else:
-				#Lendo a password correspondente 
-				password_db = query['password_hash'][0]
-				#Verifica se as senhas são iguais
-				if self.verify_hash(password_json, password_db):
-					return True
-				else:
-					raise ValueError('Error: different passwords. '  + str(e))
-		except:
-			raisa ValueError('Error: user not found. ' + str(e))
-
+				raise ValueError('Error: different passwords. '  + str(e))
+		
 '''
 auth = Authentication(table = 'users')
 
