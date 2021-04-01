684d6f80d8b239e2b97ef93d623ccea4146fc9b3
Matheus Cortez <49080775+matheus-cortez@users.noreply.github.com>
2021-04-01 12:06:39 -0300

    Update update_db.py

--- container-updatedb/update_db.py
+++ container-updatedb/update_db.py
@@ -14,7 +14,7 @@ from classes.manage_db import Manage_db
 
 #Instanciando as classes
 lop = Lop()		
-psql = Manage_db(database = 'dataviewer_bd')
+psql = Manage_db(database = 'dataviewer_db')
 
 #Essa função verifica a base de dados e definirá a data em que
 #será realizada a consulta dessa tabela. Caso a tabela esteja vazia
