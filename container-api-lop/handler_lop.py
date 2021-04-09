1df82698393854a728ad0d1fb40e8453db8fe4bf
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2021-04-09 14:24:40 -0300

    Add files via upload

--- container-api-lop/handler_lop.py
+++ container-api-lop/handler_lop.py
@@ -7,24 +7,26 @@ import re
 from datetime import datetime
 from classes.manage_lop import Lop
 from classes.manage_db import Manage_db
+from classes.manage_authentication import Authentication
 import urllib3
 import json
 import time
 import warnings
-warnings.filterwarnings("ignore")
+warnings.filterwarnings('ignore')
 urllib3.disable_warnings()
 #import sklearn
 
 #Instanciando classes
 lop = Lop()
-psql = Manage_db(database = 'dataviewer_db')
+psql = Manage_db(database = 'dataviewer_lop', port = '5432', host = os.environ['NAME_CONTAINER_LOP_DB'])
+psql_users = Manage_db(database = 'dataviewer_users', port = '5431', host = os.environ['NAME_CONTAINER_USERS_DB'])
 
 #Instanciate Flask
 app = Flask(__name__)
 
 api_cors_config = {
   'origins':'*',
-  'methods':['POST','GET','OPTIONS'],
+  'methods':['POST','GET'],
   'allow_headers':['Authorization','Content-Type']
 }
 cors = CORS(app, resource={r'/*':api_cors_config})
@@ -52,6 +54,117 @@ def get_questions():
     return json_questions
   except Exception as e:
     raise ValueError('Error: reading table questions, ' + str(e))
+
+@app.route('/post_get_graphs', methods = ['POST'])
+def post_graphs():
+  try:
+    username = request.json['username']
+    password = request.json['password']
+    id_class = request.json['id_class']
+    user_api = request.json['user_api']
+    password_user_api = request.json['password_user_api']
+  except Exception as e:
+    raise ValueError('Error: read json. ' + str(e))
+  #Verifica se o usuário mestre passado no post é o que permite
+  #cadastro no banco de dados, se for correto, o cadastro do usuário 
+  #prossegue
+  try:
+    auth.authenticate_user(user_api, password_user_api)
+  except Exception as e:
+    raise ValueError(str(e))
+  #Cria o dataframe com a turma informada
+  data = pd.DataFrame([id_class], columns = ['id_class'])
+  try:
+    condition = "WHERE id_class = '" + data.iloc[0,0] + "'"
+    df_submission = psql.search(table='submissions', condition = condition)
+    df_lop_lists = psql.search(table='lists', condition = condition)
+    df_lop_tests = psql.search(table='tests', condition = condition) 
+    df_question_data = psql.search(table='questions')
+    df_class = psql.search(table='teachers_classes')
+  except Exception as e:
+    raise ValueError('Error: consult at database. ' + str(e))
+  #Se não tiver nenhuma submissão na turma
+  if df_submission.empty:
+    raise ValueError('Error: class without data')
+  #Selecionando apenas as questões dessa turma
+  df_questions_selected = lop.select_questions(df_question_data, df_lop_lists, df_lop_tests)
+  #Dados de lista
+  try:
+    #Dados de performance
+    df_performance_list = lop.performance_list_test(df_submission, 'list', df_lop_lists, df_lop_tests, df_question_data)
+    df_performance_difficulty_list = lop.performance_difficulty_list_test(df_submission, df_questions_selected,'list')
+    df_performance_subject_list = lop.performance_subject_list_test(df_submission, df_questions_selected, 'list')
+    #Grafico 1, desempenho por turma analisando notas
+    GENL = lop.graph_more_less_list_test_class(df_performance_list, df_class, data, 70.0, 'list')
+    GTDL = lop.graph_more_less_difficulty_list_test_class(df_performance_difficulty_list, df_class, data, 70.0)
+    GTAL = lop.graph_more_less_subject_list_test_class(df_performance_subject_list, df_class, data, 70.0)
+    #Grafico 2, desempenho por aluno analisando notas
+    GTNL = lop.graph_performance_student_list_test(df_performance_list, df_class, data, 'list')
+    GEDL = lop.graph_performance_student_difficulty_list_test(df_performance_difficulty_list, df_class, data)
+    GEAL = lop.graph_performance_student_subject_list_test(df_performance_subject_list, data, df_class)
+    #Média geral dos alunos 
+    media_GTNL = lop.media_graph_performance_student_list_test(df_performance_list, df_questions_selected, 'list')
+    media_GEDL = lop.media_graph_performance_student_difficulty_list_test(df_performance_difficulty_list, df_questions_selected)
+    media_GEAL = lop.media_graph_performance_subject_list_test(df_performance_subject_list)
+  except Exception as e:
+    raise ValueError('Error: generate data lists. ' + str(e))
+  #Se não tiver nenhuma prova cadastrada  
+  if df_lop_tests.empty:
+    #Insere vazio e não realiza a geração dos dados
+    try:
+      #Grafico 1, desempenho por turma analisando notas
+      GENP = json.dumps([{}])
+      GTDP = json.dumps([{}])
+      GTAP = json.dumps([{}])
+      #Grafico 2, desempenho por aluno analisando notas
+      GTNP = json.dumps([{}])
+      GEDP = json.dumps([{}])
+      GEAP = json.dumps([{}])
+      #Média geral dos alunos 
+      media_GTNP = json.dumps([{}])
+      media_GEDP = json.dumps([{}])
+      media_GEAP = json.dumps([{}])
+    except:
+      raise ValueError('Error: insert empty in data tests')
+  #Se existe provas cadastradas, gera os dados normalmente      
+  else:
+    try:
+      df_performance_test = lop.performance_list_test(df_submission, 'test', df_lop_lists, df_lop_tests, df_question_data)
+      df_performance_difficulty_test = lop.performance_difficulty_list_test(df_submission, df_questions_selected,'test')
+      df_performance_subject_test = lop.performance_subject_list_test(df_submission, df_questions_selected, 'test')
+      #Grafico 1, desempenho por turma analisando notas
+      GENP = lop.graph_more_less_list_test_class(df_performance_test, df_class, data, 70.0, 'test')
+      GTDP = lop.graph_more_less_difficulty_list_test_class(df_performance_difficulty_test, df_class, data, 70.0)
+      GTAP = lop.graph_more_less_subject_list_test_class(df_performance_subject_test, df_class, data, 70.0)
+      #Grafico 2, desempenho por aluno analisando notas
+      GTNP = lop.graph_performance_student_list_test(df_performance_test, df_class, data, 'test')
+      GEDP = lop.graph_performance_student_difficulty_list_test(df_performance_difficulty_test, df_class, data)
+      GEAP = lop.graph_performance_student_subject_list_test(df_performance_subject_test, data, df_class)
+      #Média geral dos alunos 
+      media_GTNP = lop.media_graph_performance_student_list_test(df_performance_test, df_questions_selected, 'test')
+      media_GEDP = lop.media_graph_performance_student_difficulty_list_test(df_performance_difficulty_test, df_questions_selected)
+      media_GEAP = lop.media_graph_performance_subject_list_test(df_performance_subject_test)
+    except:
+      raise ValueError('Error: generate data tests')
+  #Histograma do dias gastos para concluir uma lista/assunto/dificuldade
+  try:
+    GTDGL = lop.graph_days_spent_list(df_submission)
+    GTDGA = lop.graph_days_spent_subject(df_submission, df_questions_selected)
+    GTDGD = lop.graph_days_spent_difficulty(df_submission, df_questions_selected)
+  except:
+    raise ValueError('Error: generate graph days spent per list/subject/difficulty')
+  #Transformando em um só json
+  name_graphs = ['"GENL"','"GENP"','"GTDL"','"GTDP"','"GTAL"','"GTAP"',
+                 '"GTNL"','"GTNP"','"GEDL"','"GEDP"','"GEAL"','"GEAP"',
+                 '"media_GTNL"','"media_GTNP"','"media_GEDL"','"media_GEDP"','"media_GEAL"','"media_GEAP"',
+                 '"GTDGL"','"GTDGA"','"GTDGD"']
+  graphs =      [GENL,GENP,GTDL,GTDP,GTAL,GTAP,
+                 GTNL,GTNP,GEDL,GEDP,GEAL,GEAP,
+                 media_GTNL,media_GTNP,media_GEDL,media_GEDP,media_GEAL,media_GEAP,
+                 GTDGL,GTDGA,GTDGD]
+  return lop.create_unique_json(name_graphs, graphs)
+
+
 
 @app.route('/get_graphs_teacher/<id_class>', methods = ['GET'])
 def get_graphs(id_class):
