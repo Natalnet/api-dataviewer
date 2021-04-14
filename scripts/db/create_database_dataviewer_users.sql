af278912d6b020cbfe10335d276b152395f8d732
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2021-04-14 09:21:22 -0300

    Add files via upload

--- scripts/db/create_database_dataviewer_users.sql
+++ scripts/db/create_database_dataviewer_users.sql
@@ -5,13 +5,13 @@ WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dataviewer_users')\ge
 
 CREATE TABLE IF NOT EXISTS users_api (
 	"username" VARCHAR(40) NOT NULL,
-	"password" VARCHAR(150) NOT NULL,
+	"password" VARCHAR(200) NOT NULL,
 	"createdAt" VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users_students (
 	"username" VARCHAR(40) NOT NULL,
-	"password" VARCHAR(150) NOT NULL,
+	"password" VARCHAR(200) NOT NULL,
 	"user" VARCHAR(70) NOT NULL,
 	"name" VARCHAR(100) NOT NULL,
 	"registration" VARCHAR (30) NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS users_students (
 
 CREATE TABLE IF NOT EXISTS users_teachers (
 	"username" VARCHAR(40) NOT NULL,
-	"password" VARCHAR(150) NOT NULL,
+	"password" VARCHAR(200) NOT NULL,
 	"name_teacher" VARCHAR(100) NOT NULL,
 	"id_teacher" VARCHAR(36) NOT NULL, 
 	"email" VARCHAR(100) NOT NULL,
