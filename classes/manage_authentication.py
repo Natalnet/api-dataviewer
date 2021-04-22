e36fdbcbea6fed9ab869b30e707aa4e1e69b3f3a
Matheus Cortez <49080775+matheus-cortez@users.noreply.github.com>
2021-04-22 17:40:45 -0300

    Update manage_authentication.py

--- classes/manage_authentication.py
+++ classes/manage_authentication.py
@@ -2,7 +2,7 @@ from passlib.hash import pbkdf2_sha512
 from classes.manage_db import Manage_db
 import pandas as pd
 
-psql = Manage_db(database = 'dataviewer_users', port = '5432', host = 'db-lop')
+psql = Manage_db(database = 'dataviewer_users', port = '5432', host = 'db-users')
 #psql = Manage_db(database = 'dataviewer_users', port = '5432', host = 'localhost')
 
 class Authentication:
