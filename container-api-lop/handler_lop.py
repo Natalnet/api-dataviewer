a821097eaeeeccd1c1814b9f1cc79fad8b2c2e4d
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2021-05-11 12:53:34 -0300

    Retirei o debug

--- container-api-lop/handler_lop.py
+++ container-api-lop/handler_lop.py
@@ -265,7 +265,7 @@ def get_graphs(id_class):
 
 def main():
   port = int(os.environ.get('PORT', 5000))
-  app.run(host = '0.0.0.0', port = port,debug=False)   
+  app.run(host = '0.0.0.0', port = port)   
 
 if __name__ == '__main__':
   main()
