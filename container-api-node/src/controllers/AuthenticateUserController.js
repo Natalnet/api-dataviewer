5f698bacb8bfa18ae4968ad27add8861afebc2a7
raphaelramosds <raphael.opensource@gmail.com>
2022-02-10 21:05:33 -0300

    Controller de autenticação

--- container-api-node/src/controllers/AuthenticateUserController.js
+++ container-api-node/src/controllers/AuthenticateUserController.js
@@ -6,9 +6,11 @@ class AuthenticateUserController {
 
   async handle(req, res) {
 
-    const { email } = req.body;
-
+    const { email, token } = req.body;
     const user = await AuthenticateUserService.execute(email);
+
+    // colocar token nos cookies
+    res.cookie("session-token", token);
 
     return res.status(200).json(user);
     
--- container-api-node/src/routes.js
+++ container-api-node/src/routes.js
@@ -2,14 +2,12 @@ const { Router } = require("express");
 const AuthenticateUserController = require("./controllers/AuthenticateUserController");
 
 const ListGraphsController = require("./controllers/ListGraphsController");
-const LoginController = require("./controllers/LoginController");
 const ensureTokenIntegrity = require("./middlewares/ensureTokenIntegrity");
 
 const router = Router();
 
 // view de login
 router.get("/login", (req, res) => { res.render("login"); });
-router.post("/login", LoginController.handle);
 
 // autenticar usuário
 router.post("/auth", AuthenticateUserController.handle);
