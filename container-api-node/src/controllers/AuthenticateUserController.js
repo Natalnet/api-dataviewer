241d1ffa2571a3601c0c4e5f4a29cff09bcd7afb
raphaelramosds <raphael.opensource@gmail.com>
2022-02-12 20:06:16 -0300

    Questões de segurança envolvendo cookies

--- container-api-node/src/controllers/AuthenticateUserController.js
+++ container-api-node/src/controllers/AuthenticateUserController.js
@@ -13,18 +13,14 @@ class AuthenticateUserController {
 
     // colocar token e id do usuário nos cookies
     if (user.length != 0) {
-      
       res.cookie("APINJS_AUTH", token);
       res.cookie("APINJS_UID", user[0]._id)
-
     } 
     
     else {
-
       // evitar que o cookie de um usuário autenticado seja usado por outro que não é
       res.clearCookie("APINJS_AUTH");
       res.clearCookie("APINJS_UID");
-
     }
 
     return res.status(200).json(user);
