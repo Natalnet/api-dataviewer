61c992e72d2bc19cb0dc9e385ac5e547c69655a2
raphaelramosds <raphael.opensource@gmail.com>
2022-03-04 08:41:40 -0300

    Documentação

--- container-api-node/src/routes/classes.routes.js
+++ container-api-node/src/routes/classes.routes.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
-const FetchClassesController = require("../controllers/FetchClassesController");
+const ListClassesController = require("../controllers/ListClassesController");
 const ensureTokenIntegrity = require("../middlewares/ensureTokenIntegrity");
 
 const classes = Router();
 
-classes.get("/:id_teacher", ensureTokenIntegrity, FetchClassesController.handle);
+classes.get("/:id_teacher", ensureTokenIntegrity, ListClassesController.handle);
 
 module.exports = classes;
